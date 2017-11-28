33c27745fe9736d875dfb6845ef13790defc0087
Florence Bolsée <florence.bolsee@gmail.com>
2017-11-28 11:45:03 +0100

    anim feature

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
-const name = "florence";
+var burger = {
+  classAnim: "anim3",
+  classAnimBack: "animBack",
+  animFunction: function(){
+    $(".burger").on('click', function(ev){
+      if(this.classAnim === "anim3"){
+        this.classAnimBack = "animBack3";
+      }
+      if($(ev.currentTarget).hasClass(this.classAnim) === true){
+        $(ev.currentTarget).removeClass(this.classAnim);
+        $(ev.currentTarget).addClass(this.classAnimBack);
+      } else {
+        $(ev.currentTarget).removeClass(this.classAnimBack);
+        $(ev.currentTarget).addClass(this.classAnim);
+      }
+    }.bind(this));
+  }
+};
+
+burger.animFunction();
